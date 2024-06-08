b0068d112067704a88dba000e269a0203c137154
Andrey <smail2204973@gmail.com>
2024-06-08 17:38:59 +0300

    Created login activity

--- app/src/main/java/com/example/cinemaapp2/IntroActivity.java
+++ app/src/main/java/com/example/cinemaapp2/IntroActivity.java
@@ -2,6 +2,9 @@ package com.example.cinemaapp2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +12,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+        Button enterBtn = findViewById(R.id.enter_btn);
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/cinemaapp2/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.cinemaapp2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userEdt, passEdt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_layout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initView();
+    }
+
+    private void initView() {
+        userEdt = findViewById(R.id.username);
+        passEdt = findViewById(R.id.password);
+        Button loginBtn = findViewById(R.id.login_button);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please, enter your login and password above", Toast.LENGTH_SHORT).show();
+                }
+                else if (userEdt.getText().toString().equals("1") && passEdt.getText().toString().equals("1"))
+                {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Your username or password is incorrect, try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
