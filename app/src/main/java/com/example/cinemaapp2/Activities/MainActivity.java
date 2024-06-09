37a86ef7aba7b8f2a3c3b339bcc7a6ecd57b9020
Andrey <smail2204973@gmail.com>
2024-06-09 17:51:28 +0300

    Implemented api and logic for popular movies screen

--- app/src/main/java/com/example/cinemaapp2/Activities/MainActivity.java
+++ app/src/main/java/com/example/cinemaapp2/Activities/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.cinemaapp2.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -11,19 +13,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.cinemaapp2.Adapters.FilmListAdapter;
 import com.example.cinemaapp2.Adapters.SliderAdapters;
+import com.example.cinemaapp2.Domain.ListFilm;
 import com.example.cinemaapp2.Domain.SliderItems;
 import com.example.cinemaapp2.R;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapterBestMovies, AdapterUp, adapterCategory;
+    private RecyclerView recyclerViewBestMovies, recyclerViewUp, recyclerViewCategory;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest, mStringRequest2, mStringRequest3;
+    private ProgressBar loading, loading2, loading3;
     private ViewPager2 viewPager2;
     private Handler slideHandler = new Handler();
 
@@ -39,29 +57,47 @@ public class MainActivity extends AppCompatActivity {
         });
         initView();
         banners();
+        sendRequests();
     }
 
+    private void sendRequests() {
+        mRequestQueue = Volley.newRequestQueue(this);
+        loading.setVisibility(View.VISIBLE);
+        mStringRequest = new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=1", response -> {
+            Gson gson = new Gson();
+            loading.setVisibility(View.GONE);
+            ListFilm items = gson.fromJson(response, ListFilm.class);
+            adapterBestMovies = new FilmListAdapter(items);
+            recyclerViewBestMovies.setAdapter(adapterBestMovies);
+
+        }, error -> {
+            loading.setVisibility(View.GONE);
+            Log.d("error", error.toString());
+        });
+
+        mRequestQueue.add(mStringRequest);
+    }
     private void banners() {
-        List<SliderItems> sliderItems= new ArrayList<>();
+        List<SliderItems> sliderItems = new ArrayList<>();
         sliderItems.add(new SliderItems(R.drawable.wide));
         sliderItems.add(new SliderItems(R.drawable.wide1));
         sliderItems.add(new SliderItems(R.drawable.wide3));
 
 
-
         viewPager2.setAdapter(new SliderAdapters(sliderItems, viewPager2));
         viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
         viewPager2.setOffscreenPageLimit(3);
-        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_ALWAYS);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
 
 
-        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
         compositePageTransformer.addTransformer(new MarginPageTransformer(40));
         compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
             @Override
             public void transformPage(@NonNull View page, float position) {
-                float r=1-Math.abs(position);
-                page.setScaleY(0.85f+r*0.15f);
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
             }
         });
 
@@ -69,8 +105,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager2.setCurrentItem(1);
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onPageSelected(int position)
-            {
+            public void onPageSelected(int position) {
                 super.onPageSelected(position);
                 slideHandler.removeCallbacks(sliderRunnable);
             }
@@ -78,30 +113,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private Runnable sliderRunnable=new Runnable() {
+    private Runnable sliderRunnable = new Runnable() {
         @Override
         public void run() {
-            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
         }
     };
 
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         super.onPause();
         slideHandler.removeCallbacks(sliderRunnable);
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         slideHandler.postDelayed(sliderRunnable, 2000);
     }
 
 
-    private void initView()
-    {
-        viewPager2=findViewById(R.id.viewPagerSlider);
+    private void initView() {
+        viewPager2 = findViewById(R.id.viewPagerSlider);
+        recyclerViewBestMovies=findViewById(R.id.view1);
+        recyclerViewBestMovies.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewUp=findViewById(R.id.view2);
+        recyclerViewUp.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewCategory=findViewById(R.id.view3);
+        recyclerViewCategory.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        loading=findViewById(R.id.progressBar1);
+        loading2=findViewById(R.id.progressBar2);
+        loading3=findViewById(R.id.progressBar3);
     }
 }
